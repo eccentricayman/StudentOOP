5a7ace0148d6b81ac8fa8657f64273d3fab39f5f
eccentricayman <ayman11432@live.com>
2019-06-04 11:36:26 -0400

    completeted test_pointers

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -4,16 +4,57 @@
 using namespace std;
 
 void inc_by_value(int n) {
-    ++n;
+	n += 1;
 }
 
 
-void inc_with_pointer(int* n) {
-    ++(*n);
+void inc_with_pointer(int* iptr) {
+	(*iptr)++;
 }
 
 
 void inc_with_reference(int& n) {
-    ++n;
+	n++;
 }
 
+
+Thing** create_array_of_things(int n) {
+	Thing** tarr = new Thing*[n];
+	for (int a = 0 ; a < n ; a++) {
+		tarr[a] = new Thing(a);
+	}
+    return tarr;
+}
+
+void print_all_things(Thing** things, int n) {
+	for (int a = 0 ; a < n ; a++) {
+		cout << things[a] -> val << " ";
+	}
+	cout << endl;
+}
+
+void double_all_things(Thing** things, int n) {
+	for (int a = 0 ; a < n ; a++) {
+		things[a] -> val *= 2;
+	}
+}
+
+void delete_all_things(Thing** things, int n) {
+	for (int a = 0 ; a < n ; a++) {
+		things[a] = nullptr;
+	}
+	//things = nullptr;
+}
+
+
+void assignTA(Student& s, Student& ta) {
+	s.ta = &ta;
+}
+
+void printTAs(vector<Student> students) {
+	for (size_t a = 0 ; a < students.size() ; a++) {
+		cout << students[a].name << " -> "
+			 << students[a].ta -> name
+			 << endl;
+	}
+}
