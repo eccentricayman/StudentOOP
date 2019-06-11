7576ea51d5fcef09f245cc727e0cfd608cb323cd
eccentricayman <ayman11432@live.com>
2019-06-11 12:14:51 -0400

    finished complex

---
+++ my_code/complex.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <istream>
+#include <iomanip>
+#include "complex.h"
+
+using namespace std;
+
+bool operator== (const Complex& c1, const Complex& c2) {
+	if (c1.get_real() == c2.get_real() && c1.get_imag() == c2.get_imag()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool operator != (const Complex& c1, const Complex& c2) {
+	if (c1 == c2) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+ostream& operator<< (ostream& os, const Complex& c) {
+    /*
+     * Outputting a `Complex` instance, while illustrating some of the
+     * capabilities of I/O streams: `setprecision` gives us a fixed
+     * number of decimal places, while `showpos` turns on the plus
+     * sign for positive numbers.
+     * */
+    os << setprecision(10) << c.real << showpos
+         << c.imag << "i" << noshowpos << endl;
+    return os;
+}
+
+
+/*
+ * Read a `Complex` number from an input stream.
+ * */
+istream& operator>> (istream& is, Complex& c) {
+    is >> c.real >> c.imag;
+    return is;
+}
+
+
+Complex::Complex(double real, double imag)
+            : real{real}, imag{imag} {}
+
+
+Complex::operator bool() const {
+    return ((real != 0) || (imag != 0));
+}
+
+Complex& Complex::operator++() {
+    ++real;
+    return (*this);
+}
+
+Complex Complex::operator++(int dummy) {
+    Complex temp(*this);
+    real++;
+    return temp;
+}
+
+Complex Complex::operator+(const Complex& c) {
+    Complex sum{real + c.real, imag + c.imag};
+    return sum;
+}
+
+Complex& Complex::operator--() {
+    --real;
+    return (*this);
+}
+
+Complex Complex::operator--(int dummy) {
+    Complex temp(*this);
+    real--;
+    return temp;
+}
+
+Complex Complex::operator-(const Complex& c) {
+    Complex sum{real - c.real, imag - c.imag};
+    return sum;
+}
+
+double Complex::get_real() const {
+    return real;
+}
+
+double Complex::get_imag() const {
+    return imag;
+}
+
+Complex Complex::operator*(const int i) {
+    Complex prod(real * 2, imag * 2);
+    return prod;
+}
