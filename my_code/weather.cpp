9140f8a4c2a913cae945b848b9c08d744ce00db5
eccentricayman <ayman11432@live.com>
2019-06-12 13:12:14 -0400

    done with weather update

--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -49,3 +49,68 @@ void Weather::set_rating(int new_rating) {
 void Weather::add_reading(WReading wr) {
 	wreadings.push_back(wr);
 }
+
+//const double F_TO_C = 5 / 9;
+const double C_TO_F = 9 / 5;
+
+Image::Image(int w, int h, std::string flnm)
+    : width(w), height(h)
+{
+    filename = flnm;
+    image_buf = new char[image_sz()];
+}
+
+// copy constructor:
+Image::Image(const Image& img2) {
+	copy_fields(img2);
+}
+
+// destructor
+Image::~Image() {
+	if (image_buf != nullptr) {
+		delete[] image_buf;
+	}
+}
+
+// assignment operator:
+Image& Image::operator=(const Image& img2) {
+	if (this == &img2) {
+		return *this;
+	}
+	else {
+		if (image_buf != nullptr) {
+			delete[] image_buf;
+		}
+		copy_fields(img2);
+		return *this;
+	}
+}
+
+int Image::image_sz() {
+    return width * height;
+}
+
+
+void Image::copy_fields(const Image& img2) {
+	width = img2.width;
+	height = img2.height;
+	filename = img2.filename;
+	//using our image_sz cause same width/height
+	image_buf = new char[image_sz()];
+	for (int a = 0 ; a < image_sz() ; a++) {
+		image_buf[a] = img2.image_buf[a];
+	}
+}
+
+
+    /*
+     * Setting `display() = 0` here makes this an abstract
+     * class that can't be implemented.
+     * */
+string Image::display(std::string s) {
+    return "Displaying image " + s;
+}
+
+double WReading::get_tempF() {
+    return (temperature * C_TO_F) + 32;
+}
--- my_code/weather.h
+++ my_code/weather.h
@@ -5,6 +5,41 @@
 #include <vector>
 #include "date.h"
 
+/*
+ * This will be the base class for all types of images.
+ * */
+class Image {
+ public:
+    Image(int w, int h, std::string flnm);
+    // copy constructor:
+    Image(const Image& img2);
+    ~Image();
+    Image& operator=(const Image& img2);
+    int image_sz();
+
+    /*
+     * Setting `display() = 0` here makes this an abstract
+     * class that can't be implemented.
+     * */
+    std::string display(std::string s);
+    /*
+     * If we don't want virtual method lookup, we
+     * could just declare:
+     * void display();
+     * */
+
+    int get_height() { return height; }
+    int get_width() { return width; }
+
+ private:
+    int width;
+    int height;
+    std::string filename;
+    char* image_buf;
+    void copy_fields(const Image& img2);
+};
+
+
 struct GPS {
     double latitude;
     double longitude;
@@ -14,6 +49,7 @@ struct GPS {
 
 std::ostream& operator<<(std::ostream& os, const GPS& gps);
 
+
 class WReading {
     friend std::ostream& operator<<(std::ostream& os, const WReading& wr);
  public:
@@ -21,6 +57,9 @@ class WReading {
         date(dt), temperature(temp), humidity(hum), windspeed(ws)
     {
     }
+
+    double get_tempF();
+    double get_tempC() { return temperature; }
 
  private:
     Date date;
